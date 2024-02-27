fd99a009caaeab74998a4491cd83b1a55681c74d
balusena <balusena12@gmail.com>
2024-02-27 15:53:25 +0000

    roboshop kubernetes files

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
-aws ssm describe-parameters
+for param in ${PARAMETERS} ; do
+  SHELL_VAR=$(echo $param | awk -F , '{print $1}')
+  PARAM=$(echo $param | awk -F , '{print $2}')
+  PASS=${aws ssm get-parameter --name $param --with-decryption --query 'Parameter.Value' --output text}
+  echo export $SHELL_VAR=$PASS >>params
+done
+
+cat params
 
